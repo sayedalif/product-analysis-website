3fe81cf9e57cd3b0728a5967b7cf05217d27a86f
sayedalif <sayedalif18@gmail.com>
2022-04-04 13:35:18 +0600

    all components added

---
+++ src/components/Header/HeaderNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderNav = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <nav></nav>
+    </div>
+  );
+};
+
+export default HeaderNav;
