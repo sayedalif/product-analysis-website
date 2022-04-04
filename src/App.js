391f7c7bcd227179bbea3e875a1c6afe6c5c556d
sayedalif <sayedalif18@gmail.com>
2022-04-04 23:17:53 +0600

    nav-section done routes and link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import HeaderNav from './components/Header/HeaderNav';
+import Home from './components/Home/Home';
+import Review from './components/Review/Review';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import DashBoard from './components/DashBoard/DashBoard';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav></HeaderNav>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/dashboard" element={<DashBoard></DashBoard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/HeaderNav.js
+++ src/components/Header/HeaderNav.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HeaderNav.css';
 
 const HeaderNav = () => {
   return (
     <div>
-      <h1>hello</h1>
-      <nav></nav>
+      <nav className="header-nav">
+        <Link className="nav-link" to="/home">
+          Home
+        </Link>
+        <Link className="nav-link" to="/review">
+          Review
+        </Link>
+        <Link className="nav-link" to="/dashboard">
+          Dashboard
+        </Link>
+        <Link className="nav-link" to="/blogs">
+          Blogs
+        </Link>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </nav>
     </div>
   );
 };
