6b7306b9d030ea4747572905de2a4004a2473c10
sayedalif <sayedalif18@gmail.com>
2022-04-05 14:20:30 +0600

    minor changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import shoe from './shoe.jpg';
 import './Home.css';
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div className="header">
-      <div className="header-title">
-        <h1>Your next shoe</h1>
-        <h1 className="sec-title">Your best shoe</h1>
-        <p>This show is a showcase of the best shoes in the world.</p>
-      </div>
+    <div>
+      <div className="header">
+        <div className="header-title">
+          <h1>Your next shoe</h1>
+          <h1 className="sec-title">Your best shoe</h1>
+          <p>This show is a showcase of the best shoes in the world.</p>
+        </div>
 
-      <div>
-        <img src={shoe} alt="" height={200} width={200} />
+        <div>
+          <img src={shoe} alt="" height={200} width={200} />
+        </div>
       </div>
+      <button className="rev-btn">
+        <Link className="rev-link" to="/review">
+          Show all Review
+        </Link>
+      </button>
     </div>
   );
 };
